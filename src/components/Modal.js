7e5355b322d03c70201d2f8b1ab767ff0c3a66e4
hsh411 <pixeldot12@gmail.com>
2021-05-29 20:44:23 +0900

    modal upload

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,42 @@
-import { useState } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 
 function Modal({ showModal, setShowModal }) {
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const closeKey = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", closeKey);
+    return () => document.removeEventListener("keydown", closeKey);
+  }, [closeKey]);
+
   if (!showModal) return null;
-  return <div id="modal_box">나는 모달</div>;
+
+  return (
+    <div id="modal_box">
+      <div className="modal_contents">
+        <button
+          className="modal_close_btn"
+          onClick={() => setShowModal((modal) => !modal)}
+        >
+          &#10005;
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
